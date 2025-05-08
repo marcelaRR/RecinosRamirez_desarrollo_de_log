9f191aa80be0b36ce2d73512f5b7b5265d75073e
Marcela <marcelaarecinos27@gmail.com>
2025-05-07 20:22:40 -0600

    IINICO DE SESION PARTE 1

---
+++ core/secure/ips.php
@@ -0,0 +1,8 @@
+<?php
+$rango = array(
+    "127.0.0.1/32", //permite unicamente la direecion localhost
+    "192.168.1.0/24",//permite todos los hos fr la red 192.168.1*
+    "10.0.0*",//permite todos los host de la rws 10.0.0.x
+    "172.16.10.1-172.16.10.50"//permitre el rango de ip especifico
+);
+?>
